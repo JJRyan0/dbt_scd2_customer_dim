acbccd0a65262eb823791c782a51232c256771e1
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-01 10:57:55 -0700

    Update update_dim_customer.sql

--- models/macros/update_dim_customer.sql
+++ models/macros/update_dim_customer.sql
@@ -1,1 +1,13 @@
+--post hook macro for dim_customer incremental update
+
+{% macro update_dim_customer(dim_customer) %}
+update {{ this }} t
+set
+  t.is_current = false,
+  t.effective_end = ccr.updated_dt
+from {{ ref('stg_customer') }} ccr
+where t.customer_id = ccr.customer_id
+and t.is_current = TRUE;
+and md5(coalesce(t.name, '') || coalesce(t.address, '')) <> ccr.hash_key
+and cr.updated_dt > coalesce(t.effective_start, '1900-01-01')
 
