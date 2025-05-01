e8ad9970463889c0bb40af929834cc6b2f8b23cc
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-01 11:39:21 -0700

    Update raw_customer.sql

--- raw_ddl/raw_customer.sql
+++ raw_ddl/raw_customer.sql
@@ -1,6 +1,6 @@
 create schema if not exists raw_schema_customer
 
-create table raw_schema_customer.customer(
+create table raw_schema_customer.raw_customer(
   customer_id int primary key,
   name text,
   address text,
