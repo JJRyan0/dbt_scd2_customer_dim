653dd70c1f8b30984a29dcd481013c421694eb8e
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-01 14:32:42 -0700

    Add files via upload

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -1,11 +1,13 @@
 
 -- select all rows from raw_customer source data
 with source_customer as (
-select customer_id
-        name,
-        address,
-        updated_dt,
-        md5(coalesce(name, '') || coalesce(address, '')) as hash_key
-from {{ source('raw_customer', 'raw_customer') }}
+  select 
+    customer_id,
+    name,
+    address,
+    updated_dt,
+    md5(coalesce(name, '') || coalesce(address, '')) as hash_key
+  from {{ source('raw_customer', 'raw_customer') }}
 )
+select * from source_customer
 
