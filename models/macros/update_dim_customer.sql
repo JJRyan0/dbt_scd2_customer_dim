acdd4d10841f03ad0227e28e48182282d63fd898
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-01 14:30:53 -0700

    Add files via upload

--- models/macros/update_dim_customer.sql
+++ models/macros/update_dim_customer.sql
@@ -1,13 +1,13 @@
 --post hook macro for dim_customer incremental update
 
 {% macro update_dim_customer(this) %}
-update {{ this }} t
+update {{ this }} as t
 set
   is_current = false,
   effective_end = ccr.updated_dt
-from {{ ref('stg_customer') }} ccr
+from {{ source('raw_customer', 'raw_customer') }} as ccr
 where t.customer_id = ccr.customer_id
 and t.is_current = TRUE
-and md5(coalesce(t.name, '') || coalesce(t.address, '')) <> ccr.hash_key
+and md5(coalesce(t.name, '') || coalesce(t.address, '')) <> md5(coalesce(ccr.name, '') || coalesce(ccr.address, ''))  -- Recreate hash_key logic
 and ccr.updated_dt > coalesce(t.effective_start, '1900-01-01')
 {% endmacro %}
