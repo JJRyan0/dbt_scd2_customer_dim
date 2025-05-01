bea2652e713f521dc519f7a99c826cc440c38a55
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-01 10:03:45 -0700

    Update stg_customer.sql

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -1,4 +1,11 @@
 
 -- select all rows from raw_customer source data
 with source_customer as (
-select *, md5(coalesce(name, ' ') || coalesce(
+select customer_id
+        name,
+        address,
+        updated_dt,
+        md5(coalesce(name, '') || coalesce(address, '')) as hash_key
+from {{ source('raw_schema_customer', 'raw_customer') }}
+)
+
